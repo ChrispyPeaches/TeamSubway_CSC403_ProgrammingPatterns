287851d677e481aa96e02e731b35b8d84a371b07
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2023-10-14 19:09:01 -0500

    Added Iterator Folder and code into "Source"

---
+++ source/Iterator/PrimeCheck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test;
+
+class PrimeCheck
+{
+    static void ETC(string[] args)
+    {
+        var nums = PrimeCheck(100_000_000);
+
+        foreach (var result in nums)
+        {
+            Console.WriteLine(result);
+            Console.WriteLine("Press Enter to Continue...\n");
+            Console.ReadLine();
+        }
+    }
+
+    static IEnumerable<String> PrimeCheck(int nums)
+    {    
+        for (int curNum = 0; curNum <= nums; curNum++)
+        {
+            int factors = 0;
+            for (int j = 1; j <= curNum; j++) 
+            {
+                if (curNum % j == 0)
+                {
+                    factors++;
+                }
+            }
+            if (factors == 2)
+            {
+                yield return curNum + " is a prime number! :D";
+            }
+            else
+            {
+                yield return curNum + " is NOT a prime number. >:(";
+            }
+        }
+    }
+}
---
+++ source/Iterator/PrimeCheckWithoutIteration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test;
+
+class PrimeCheckWithoutIteration
+{
+    static void Main(string[] args)
+    {
+        var nums = SlowPrimeCheck(100_000);
+
+        foreach (var result in nums)
+        {
+            Console.WriteLine(result);
+            Console.WriteLine("Press Enter to Continue...\n");
+            Console.ReadLine();
+        }
+    }
+
+    static IEnumerable<string> SlowPrimeCheck(int nums)
+    {
+        List<string> numbers = new List<string>();
+
+        for (int curNum = 0; curNum <= nums; curNum++)
+        {
+            int factors = 0;
+            for (int j = 1; j <= curNum; j++)
+            {
+                if (curNum % j == 0)
+                {
+                    factors++;
+                }
+            }
+            if (factors == 2)
+            {
+                numbers.Add(curNum + " is a Prime Number! :D");
+            }
+            else
+            {
+                numbers.Add(curNum + " is NOT a Prime Number. >:(");
+            }
+        }
+        return numbers;
+    }
+}
+
